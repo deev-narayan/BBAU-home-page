527d3f08c3c82c50f4077b6999b13fcc65e2ad45
Divyansh <svnarayan333@gmail.com>
2024-12-23 17:23:32 +0530

    2nd commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# test
+# Babasaheb Bhimrao Ambedkar University
+1. The home pge of bbau website is developed using
+- HTML
+- CSS
---
+++ extras/extra.js
@@ -0,0 +1,403 @@
+document.getElementById("nav-bar").innerHTML = `<div id="inp">
+<div id="logo">
+    <img src="https://www.bbau.ac.in/bbau/bbauLogo.png" alt="">
+</div>
+<div id="dispe" style=" height: fit-content;">
+    <div id="info">
+
+        <div id="name">
+            <p>बाबासाहेब भीमराव अम्बेडकर विश्वविद्यालय</p>
+            <p>Babasaheb Bhimrao Ambedkar University</p>
+        </div>
+        <div class="ach">
+
+            <p>Accredited 'A++' Grade by NAAC (2023)</p>
+            <p>NIRF Rank 33 (2024)
+            </p>
+            <p>ISO 14001:2015</p>
+        </div>
+    </div>
+        <div id="menu">
+        <i class="ri-menu-line"></i>
+        </div>
+    <ul id="nav-links">
+        <li><li class="navitm"><i class="fa-solid fa-house"></i></a></li>
+        <li><li class="navitm">About<i class="ri-arrow-drop-down-fill"></i></a>
+            <ul class="hidde-nav">
+                <ul><span>About</span>
+                <a>University</a>
+                <a>Vision & Mission</a>
+                <a>Act & Statutes</a>
+                <a>Academic Ordinance</a>
+                <a>Kulgeet</a>
+                </ul>
+                <ul><span>Contact</span>
+                <a>University Gallery's</a>
+                <a>Event Gallery</a>
+                <a>Press/Media Gallery</a>
+                <a> Video Gallery</a>
+                </ul>
+            </ul>
+        </li>
+        <li><li class="navitm">Administration<i class="ri-arrow-drop-down-fill"></i></a>
+            <ul class="hidde-nav">
+                <ul><span>Statutory Bodies</span>
+                    <a>Board of Management</a>
+                    <a>Academic Council</a>
+                    <a>Planning Board</a>
+                    <a>Finance Committee</a>
+                </ul>
+                <ul><span>Administration</span>
+                    <a>Visitor</a>
+                    <a>Chancellor</a>
+                    <a>Vice Chancellor</a>
+                    <a>Registrar</a>
+                </ul>
+                <ul>
+                <a>Finance Officer</a>
+                <a>Controller Of Examination</a>
+                <a>Proctor</a>
+                <a>Dean Student Welfare</a>
+                <a>Research & Development</a>
+                <a>Director IQAC</a>
+                <a>Hindi Cell</a>
+                </ul>
+                <ul>
+                <a>Librarian</a>
+                <a>Dean Academic affairs</a>
+                <a>Alumni Cell</a>
+                <a>Chief Vigilance Officer</a>
+                <a>Deans of School</a>
+                <a>Heads of Department</a>
+                <a>Other / Important Centre's</a>
+                </ul>
+            </ul>
+        </li>
+        <li><li class="navitm">Academics<i class="ri-arrow-drop-down-fill"></i></a>
+            <ul class="hidde-nav">
+                <ul><span>Academics</span>
+                <a>Dean Academic Affairs</a>
+                <a>M.Phil / Ph.D Regulations</a>
+                <a>MOU</a>
+                <a>Consultancy</a>
+                <a>Course folder</a>
+                <a>Academic Calender</a>
+                <a>Holidays</a>
+                <a>Institutional Academic Integrity Panel (IAIP)</a>
+                <a>Centre for Industry Institution Partnership Program (CIIPP)</a>
+                </ul>
+                <div>
+                    <ul><span>IQAC</span>
+                    <a>About IQAC</a>
+                    </ul>
+                    <ul><span>Academic Flexibility</span>
+                    <a>Foundation Course</a>
+                    <a>Choice Based Credit System</a>
+                    </ul>
+                    <ul><span>Publications</span>
+                    <a>Annual Reports</a>
+                    <a>BBAU Newsletter</a>
+                    <a>Journals</a>
+                    </ul>
+                </div>
+            </ul>
+        </li>
+        <li><li class="navitm">IQAC</a></li>
+        <li><li class="navitm">School & Dept.<i class="ri-arrow-drop-down-fill"></i></a>
+            <ul class="hidde-nav">
+                <ul><span>School</span>
+                <a>Ambedkar School of Social Sciences</a>
+                <a>Life Sciences</a>
+                <a>Information Science & Technology</a>
+                <a>Earth & Environmental Sciences</a>
+                <a>Education</a>
+                <a>Home Science</a>
+                </ul>
+                <ul><span>Legal Studies</span>
+                <a>Management and Commerce</a>
+                <a>Physical & Decision Science</a>
+                <a>Languages and Literature</a>
+                <a>Media and Communication</a>
+                <a>Agricultural Sciences & Technology</a>
+                </ul>
+                <div>
+                    <ul>
+                    <a>Pharmaceutical Sciences</a>
+                    <a>Sanskrit & Indic Studies</a>
+                    <a>School of Vocational & Futuristic Studies</a>
+                    <a>School of Yoga Naturopathy & Cognitive Studies</a>
+                    <a>School of Performing Arts</a>
+                    <a>School of Engineering & Technology</a>
+                    </ul>
+                    <ul><span>Centre's & Institute</span>
+                    <a>Satellite Centre, Amethi</a>
+                    <a>University Institute of Engineering & Technology</a>
+                    <a>Other / Important Centre's</a>
+                    </ul>
+                </div>
+            </ul>
+        </li>
+        <li><li class="navitm">Programme<i class="ri-arrow-drop-down-fill"></i></a>
+            <ul class="hidde-nav">
+                <div>
+                    <ul><span>Doctoral</span>
+                    <a>Ph.D</a>
+                    <a>M.Phil</a>
+                    </ul>
+                    <ul><span>Integrated Courses</span>
+                    <a>B.Sc.-M.Sc</a>
+                    <a>BBA-LLB</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul><span>Post Graduate</span>
+                    <a>M.Sc..</a>
+                    <a>M.Tech..</a>
+                    <a>M.A...</a>
+                    <a>LLM</a>
+                    <a>M.Pharm</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul><span>Under Graduate</span>
+                    <a>B.Tech.</a>
+                    <a>BBA</a>
+                    <a>B.Sc.</a>
+                    </ul>
+                    <ul><span>Others</span>
+                    <a>Diploma</a>
+                    <a>Certificate</a>
+                    </ul>
+                </div>
+            </ul>
+        </li>
+        <li><li class="navitm">Research<i class="ri-arrow-drop-down-fill"></i></a>
+            <ul class="hidde-nav">
+                <div>
+                    <ul><span>Director Research & Development</span>
+                    <a>Research & Development</a>
+                    <a>Ph.D Awarded</a>
+                    <a>Ongoing Ph.D Research</a>
+                    <a>M.Phil / Ph.D Regulations</a>
+                    <a>Awards and Honors</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul><span>Projects</span>
+                    <a>Completed & On-going</a>
+                    <a>Guidelines for Research Project</a>
+                    </ul>
+                    <ul><span>Committee</span>
+                    <a>Institutional Ethics Committee</a>
+                    <a>Institutional Biosafety Committee</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul><span>Publication</span>
+                    <a>Last 5 Years</a>
+                    <a>Citations</a>
+                    </ul>
+                    <ul><span>Patents</span>
+                    <a>University Patent Cell</a>
+                    </ul>
+                </div>
+            </ul>
+        </li>
+        <li><li class="navitm">Students<i class="ri-arrow-drop-down-fill"></i></a>
+            <ul class="hidde-nav">
+                <div>
+                    <ul><span>Major Links</span>
+                    <a>Office for International Affairs</a>
+                    <a>Students Welfare</a>
+                    <a>Students Council</a>
+                    <a>SC/ST Cell</a>
+                    <a>OBC Cell</a>
+                    <a>Central Placement Cell</a>
+                    <a>Information Guidance Bureau</a>
+                    <a>Centre for Personality Development</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul><span>Portals</span>
+                    <a>Semester Result</a>
+                    <a>Grievance Redressal Cell</a>
+                    <a>Students Feedback Portal</a>
+                    <a>SC/ST, OBC, Disable & Minority Complaint Portal</a>
+                    <a>Students Counsellings Centre</a>
+                    <a>Internal Complaints Committee</a>
+                    <a>e-samadhan</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul><span>Other Links</span>
+                    <a>National Cadet Corps</a>
+                    <a>National Service Scheme</a>
+                    <a>Scholarships</a>
+                    <a>Sports</a>
+                    <a>e-Learning Resources</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul><span>Other Portals</span>
+                    <a>Skills test for internship and jobs</a>
+                    <a>UGC Online Student's grievance Redressal System</a>
+                    </ul>
+                    <ul>
+                    <span>Downloads</span>
+                    <a>Downloads for Students</a>
+                    </ul>
+                </div>
+            </ul>
+        </li>
+        <li><li class="navitm">Infrastructure & Faculties<i class="ri-arrow-drop-down-fill"></i></a>
+            <ul class="hidde-nav">
+                <div>
+                    <ul><span>Physical Infrastructure</span>
+                    <a>University Sophisticated Instrumentation Centre</a>
+                    <a>Library</a>
+                    <a>Hostel's</a>
+                    <a>Health Centre</a>
+                    <a>Media Centre</a>
+                    <a>Guest House</a>
+                    <a>Sports</a>
+                    </ul>
+                    <ul><span>Auditorium</span>
+                    <a>Atal Bihari Vajpayee Auditorium & Conference Hall's</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul><span>IT Infrastructure</span>
+                    <a>University Computer Centre</a>
+                    <a>National Knowledge Network</a>
+                    </ul>
+                    <ul>
+                        <span>Students</span>
+                        <a>Central Placement Cell</a>
+                        <a>Information Guidance Bureau</a>
+                        <a>Residential Coaching Academy</a>
+                        <a>Remedial Coaching Program</a>
+                        <a>Dr. Ambedkar Centre of Excellence</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul><span>Others</span>
+                    <a>Procedures and poacies</a>
+                    <a>Basic Faciaty for Women</a>
+                    <a>Post Office</a>
+                    <a>Bank</a>
+                    <a>ATM</a>
+                    </ul>
+                </div>
+            </ul>
+        </li>
+        <li><li class="navitm">Login</a>
+            <ul class="hidde-nav">
+                <div>
+                    <ul><span> Login </span>
+                    <a> Internet User Authentication</a>
+                    <a> Email (@bbau.ac.in)</a>
+                    <a> Email(@email.bbau.ac.in)</a>
+                    <a> Exam Section (ERP)</a>
+                    <a> Webex Login</a>
+                    </ul>
+                </div>
+                <div>
+                    <ul> <span>Portals</span>
+                    <a> Samarth Students Login</a>
+                    <a> UGC Grievance Redressal Portal</a>
+                    <a> Students Feedback Form</a>
+                    <a> SC/ST, OBC, Disable & Minority
+                        <br> Discrimination Complaint Portal</a>
+                    <a> Students Counsellings Centre</a>
+                    </ul>
+                </div>
+            </ul>
+        </li>
+    </ul>
+</div>
+</div>`
+
+
+document.getElementById("footer").innerHTML = `
+
+<div class="footeraddr">
+    <h1 class="footerlogo">Babasaheb Bhimrao Ambedkar University</h1>
+    <h1 class="footerlogo">बाबासाहेब भीमराव अम्बेडकर विश्वविद्यालय</h1>
+    <h2>Contact</h2>
+
+    <address>
+        Babasaheb Bhimrao Ambedkar University, <br>
+        (A Central University) <br>
+        Vidya Vihar, Raebareli Road, Lucknow - 226025 <br>
+        <br>
+        <a href="index copy.html"> Toll Free: 1800-180-5789 </a> <br>
+        <a href=""> Fax 1: +91-522-2440821 </a> <br>
+
+    </address>
+</div>
+
+<ul class="footernav">
+    <li class="navitem">
+        <h2 class="navtitle">Important Links</h2>
+
+        <ul class="navul">
+            <li><a href=""> UGC </a></li>
+            <li><a href=""> MHRD </a></li>
+            <li><a href=""> PFMS </a></li>
+            <li><a href=""> SWAYAM </a></li>
+            <li><a href=""> NAD </a></li>
+            <li><a href=""> NKN </a></li>
+            <li><a href=""> GOI Directory </a></li>
+            <li><a href=""> NCCRP </a></li>
+            <li><a href=""> Cyber Swachhta </a></li>
+            <li><a href=""> NCTE </a></li>
+
+        </ul>
+    </li>
+
+    <li class="navitem navitem--extra">
+        <h2 class="navtitle">Quick Links</h2>
+
+        <ul class="navul navulextra">
+
+
+            <li><a href=""> File Tracking System </a> </li>
+            <li><a href=""> Online mode of fee payment </a> </li>
+            <li><a href=""> NEP2020 Implementation </a> </li>
+            <li><a href=""> COVID - 19 Task Force </a> </li>
+            <li><a href=""> Academics </a> </li>
+            <li><a href=""> Video Lectures </a> </li>
+            <li><a href=""> Statutory Bodies </a> </li>
+            <li><a href=""> IQAC </a> </li>
+            <li><a href=""> NIRF </a> </li>
+            <li><a href=""> Library </a> </li>
+            <li><a href=""> BBAU - IRINS </a> </li>
+            <li><a href=""> Centres </a> </li>
+            <li><a href=""> Right to Information </a> </li>
+            <li><a href=""> Exam Related Circular & Notices </a> </li>
+            <li><a href=""> Tenders </a> </li>
+            <li><a href=""> Forms & Formats </a> </li>
+            <li><a href=""> Institutions Innovation Council </a> </li>
+            <li><a href=""> Office Notices & Circulars </a> </li>
+            <li><a href=""> Event Gallery </a> </li>
+            <li><a href=""> ARIIA 2021 </a> </li>
+            <li><a href=""> BBAU ERP: Samarth </a> </li>
+            <li><a href=""> University Works Department </a> </li>
+            <li><a href=""> Guidelines / SoP / Manual </a> </li>
+            <li><a href=""> Archive </a> </li>
+
+
+        </ul>
+    </li>
+
+
+</ul>
+
+<div class="legal">
+    <p>&copy; 2024 BBAU All rights reserved.</p>
+
+    <div class="legallinks">
+        <span>Made with Skill & <span class="heart">♥</span> Dev Turbulent</span>
+    </div>
+</div>
+`
